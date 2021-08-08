ae8550d29ebc77c2e474ef5b42562c864fe51af5
MekkyMayata <okpallannaemeka@gmail.com>
2021-08-08 23:46:39 +0100

    add s3

--- vars/s3Sync.groovy
+++ vars/s3Sync.groovy
@@ -4,13 +4,14 @@ def call(String folder_path, String s3_bucket_name, String aws_region) {
   def region = aws_region
 
   def aws_cp = "aws s3 cp ${folder_path} s3://${bucket}/ --region ${region}".execute()
+  println(aws_cp + "here")
   // long execution
-  aws_cp.waitForOrKill(5000)
+  // aws_cp.waitForOrKill(5000)
 
   // check
   def aws_s3_out = "aws s3 ls s3://${bucket}/${folder_path}/ --region ${region} --recursive".execute()
   // long execution
-  aws_s3_out.waitForOrKill(5000)
+  // aws_s3_out.waitForOrKill(5000)
 
   return aws_s3_out
 }
