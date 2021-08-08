ad6a1b4f4bac6ab3b85d7acb10d9cb4a140f668a
MekkyMayata <okpallannaemeka@gmail.com>
2021-08-09 00:26:49 +0100

    add s3

--- vars/s3Sync.groovy
+++ vars/s3Sync.groovy
@@ -6,12 +6,8 @@ def call(String folder_path, String s3_bucket_name, String aws_region) {
   def aws_cp = "aws s3 cp ${folder_path} s3://${bucket}/ --region ${region} --recursive".execute()
   def cp_buffer = new StringBuffer()
   aws_cp.consumeProcessErrorStream(cp_buffer)
-  // println("here")
-  // println(region)
-  // println(bucket)
-  // println(folder)
   println(aws_cp.text)
-  println(b.toString())
+  println(cp_buffer.toString())
   // long execution
   // aws_cp.waitForOrKill(5000)
 
@@ -19,6 +15,8 @@ def call(String folder_path, String s3_bucket_name, String aws_region) {
   def aws_s3_out = "aws s3 ls s3://${bucket}/${folder_path}/ --region ${region}".execute()
   def ls_buffer = new StringBuffer()
   aws_ls.consumeProcessErrorStream(ls_buffer)
+  println(aws_s3_out.text)
+  println(ls_buffer.toString())
   // long execution
   // aws_s3_out.waitForOrKill(5000)
 
